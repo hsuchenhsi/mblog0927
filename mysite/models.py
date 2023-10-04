15ea6f76177115e1a5c30419362e00cd086149c4
hsuchenhsi <11056020@ntub.edu.tw>
2023-10-04 15:54:49 +0800

    多了post

--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title=models.CharField(max_length=200) #標題
+    slug=models.CharField(max_length=200) #網址
+    body=models.TextField() #內容
+    pub_date=models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering=('-pub_date', ) #-反向排序
+    def __str__(self) -> str:
+        return self.title
