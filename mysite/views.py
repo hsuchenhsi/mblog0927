10d3819e287e8723c8e5ceefbd43fb45b91199d9
hsuchenhsi <11056020@ntub.edu.tw>
2023-11-22 16:21:36 +0800

    1122

--- mysite/views.py
+++ mysite/views.py
@@ -1,42 +1,56 @@
 from django.shortcuts import render
 from mysite.models import Post
 from django.http import HttpResponse
-from datetime import datetime 
+from datetime import datetime
 from django.shortcuts import redirect
-
-
 # Create your views here.
 def homepage(request):
-    posts=Post.objects.all()
-    now=datetime.now()
-    return render(request,'index.html',locals())
-
-def showpost(request,slug):
-        post=Post.objects.get(slug=slug)
-        return render(request,'post.html',locals())
-        
-        
+    posts = Post.objects.all()
+    now = datetime.now()
+    hour = now.timetuple().tm_hour
+    print(f'hour = {hour}')
+    return render(request, 'index.html', locals())
+    
+def showpost(request, slug):
+    post = Post.objects.get(slug=slug) 
+    return render(request, 'post.html', locals())
+    #select * from post where slug=%slug
+    
 import random
-
 def about(request, num=-1):
     quotes = ['今日事，今日畢',
-            '要怎麼收穫，先那麼栽',
-            '知識就是力量',
-            '一個人的個性就是他的命運']
-    
+              '要怎麼收穫，先那麼栽',
+              '知識就是力量',
+              '一個人的個性就是他的命運']
     if num == -1 or num > 4:
         quote = random.choice(quotes)
     else:
         quote = quotes[num]
-    return render(request, 'about.html', locals())      
+    return render(request, 'about.html', locals())   
 
+def carlist(request, maker=0):
+    car_maker = ['Ford', 'Honda', 'Mazda']
+    car_list = [
+        [{'model':'Fiesta', 'price': 203500},
+            {'model':'Focus','price': 605000}, 
+            {'model':'Mustang','price': 900000}],
+		[{'model':'Fit', 'price': 450000}, 
+		 {'model':'City', 'price': 150000}, 
+		 {'model':'NSX', 'price':1200000}],
+		[{'model':'Mazda3', 'price': 329999}, 
+		 {'model':'Mazda5', 'price': 603000},
+		 {'model':'Mazda6', 'price':850000}],]
 
+    maker = maker
+    maker_name =  car_maker[maker]
+    cars = car_list[maker]
+    return render(request, 'carlist.html', locals())
 
 '''
 def homepage(request):
-    posts=Post.objects.all() #selsct*from post
-    post_lists=list()
-    for counter,post in enumerate(posts):
-        post_lists.append(f'No. {counter}-{post} <br>')  #<br> 網頁中的換行
+    posts = Post.objects.all() #select * from post
+    post_lists = list()
+    for counter, post in enumerate(posts):
+        post_lists.append(f'No. {counter}-{post} <br>')
     return HttpResponse(post_lists)
 '''
